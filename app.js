947e26076b63813b3543eae2231a263fdb8fd1d1
JelloSpeedGeek <angeloj-miranda@hotmail.com>
2017-06-01 13:31:56 +1200

    converted to ejs, added database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var pg = require('pg');
+var path = require('path');
 var bodyParser = require('body-parser');
 var connectionString = process.env.DATABASE_URL||"postgres://mirandange:300323076@depot:5432/mirandange_jdbc";
 var client = new pg.Client(connectionString);
@@ -10,31 +11,36 @@ client.connect();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index', {
+		title: "hello!"    
+    });
 });
 
 app.get('/authors', function (req, res) {
-    res.sendFile(__dirname + '/views/authors.html');
+    res.render('authors', {
+    });
 });
 
 app.get('/books', function (req, res) {
-    res.sendFile(__dirname + '/views/books.html');
+    res.render('books', {
+    });
 });
 
 app.get('/genres', function (req, res) {
-    res.sendFile(__dirname + '/views/genres.html');
-});
-
-app.get('/index', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('genres', {
+    });
 });
 
 app.get('/search', function (req, res) {
-    res.sendFile(__dirname + '/views/search.html');
+    res.render('search', {
+    });
 });
 
